5aa7b32512189ad42d1de2fa774c1374b956ad7e
Mialenna <annakupriyanova04@mail.ru>
2022-11-17 11:06:17 +0700

    update clicklistener

--- NestedLayouts/app/src/main/java/com/example/nestedlayouts/MainActivity.kt
+++ NestedLayouts/app/src/main/java/com/example/nestedlayouts/MainActivity.kt
@@ -4,46 +4,53 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var button_roll = (findViewById<Button>(R.id.button_roll))
-        var textView1_1 = (findViewById<TextView>(R.id.textView1_1))
-        var textView1_2 = (findViewById<TextView>(R.id.textView1_2))
-        var textView1_3 = (findViewById<TextView>(R.id.textView1_3))
-        var textView2_1 = (findViewById<TextView>(R.id.textView2_1))
-        var textView2_2 = (findViewById<TextView>(R.id.textView2_2))
-        var textView2_3 = (findViewById<TextView>(R.id.textView2_3))
-        var textView3_1 = (findViewById<TextView>(R.id.textView3_1))
-        var textView3_2 = (findViewById<TextView>(R.id.textView3_2))
-        var textView3_3 = (findViewById<TextView>(R.id.textView3_3))
-        button_roll.setOnClickListener{
-            if (textView1_1.getText() == "1" && textView2_1.getText() == "1" && textView3_1.getText() == "1") {
-                textView1_2.setText("2")
-                textView1_1.setText(" ")
-                textView2_2.setText("2")
-                textView2_1.setText(" ")
-                textView3_2.setText("2")
-                textView3_1.setText(" ")
-            }
-            else if (textView1_2.getText() == "2" && textView2_2.getText() == "2" && textView3_2.getText() == "2") {
-                textView1_3.setText("3")
-                textView1_2.setText(" ")
-                textView2_3.setText("3")
-                textView2_2.setText(" ")
-                textView3_3.setText("3")
-                textView3_2.setText(" ")
-            }
-            else if (textView1_3.getText() == "3" && textView2_3.getText() == "3" && textView3_3.getText() == "3") {
-                textView1_1.setText("1")
-                textView1_3.setText(" ")
-                textView2_1.setText("1")
-                textView2_3.setText(" ")
-                textView3_1.setText("1")
-                textView3_3.setText(" ")
+        val button_roll = (findViewById<Button>(R.id.button_roll))
+        val list = arrayListOf<TextView>()
+        list.add(0, findViewById(R.id.textView1_1))
+        list.add(1, findViewById(R.id.textView1_2))
+        list.add(2, findViewById(R.id.textView1_3))
+        list.add(3, findViewById(R.id.textView2_1))
+        list.add(4, findViewById(R.id.textView2_2))
+        list.add(5, findViewById(R.id.textView2_3))
+        list.add(6, findViewById(R.id.textView3_1))
+        list.add(7, findViewById(R.id.textView3_2))
+        list.add(8, findViewById(R.id.textView3_3))
+        val following = 3
+        var clickCount = 0
+        var text = clickCount + 2
+
+        button_roll.setOnClickListener {
+            if (clickCount != 2) {
+                list[clickCount].setText(" ")
+                list[clickCount + following].setText(" ")
+                list[clickCount + following * 2].setText(" ")
+
+                list[clickCount + 1].setText(text.toString())
+                list[clickCount + following + 1].setText(text.toString())
+                list[clickCount + following * 2 + 1].setText(text.toString())
+
+                ++clickCount
+                ++text
+            } else {
+                list[clickCount].setText(" ")
+                list[clickCount + following].setText(" ")
+                list[clickCount + following * 2].setText(" ")
+
+                clickCount = 0
+                text = 1
+
+                list[clickCount].setText(text.toString())
+                list[clickCount + following].setText(text.toString())
+                list[clickCount + following * 2].setText(text.toString())
+
+                ++text
             }
         }
     }
